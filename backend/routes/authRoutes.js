30089009e8ff9b40de8f67b9dd2916679999f9fa
jalp <jalp@gmail.com>
2025-05-14 09:03:44 +0530

    signup

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword, role });
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
